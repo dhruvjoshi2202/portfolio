d099b5a66b042677fc7baf22c217254094e46a2a
dhruvjoshi2202 <dhruvjoshi222000@gmail.com>
2025-12-16 18:10:59 +0530

    main

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -54,7 +54,7 @@ export default function Navbar() {
 
           {/* Resume Button */}
           <Link
-            href="/Dhruv_Joshi_Resume.pdf"
+            href="/Dhruv.jpg"
             target="_blank"
             className="ml-6 px-5 py-2 bg-gradient-to-r from-blue-500 to-purple-500 text-white rounded-xl shadow-md hover:scale-105 transition-transform"
           >
